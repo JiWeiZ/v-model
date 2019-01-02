1f2bfb7a4fecd53563d4aeaa07d1165b2ca169df
happysir <552446860@qq.com>
2019-01-02 19:14:12 +0800

    before directives

---
+++ src/components/editor.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <tag-box :maxTagNum="6">新建一级标签（最多6个）</tag-box>
+  </div>
+</template>
+
+<script>
+import tagBox from './tagBox'
+
+export default {
+  components: {
+    tagBox
+  }
+}
+</script>
---
+++ src/components/tagInput.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="tag-input-wrapper">
+    <input
+      type="text"
+      class="tag-input"
+      :style="{width: inputWidth+'em'}"
+      maxlength="20"
+      v-bind="$attrs"
+      v-on="inputListeners"
+      :value="tagValue"
+    ><span class="tag-input-icon"><i class="fas fa-times" @click="emitDeleteTag"></i></span>
+  </div>
+</template>
+
+<script>
+export default {
+  directives: {
+    focus: {
+      inserted (el) {
+        el.focus()
+      }
+    }
+  },
+  model: {
+    prop: 'tagValue'
+  },
+  props: {
+    tagValue: String
+  },
+  computed: {
+    // input框宽度随字数自适应
+    inputWidth () {
+      // 考虑中英文字符宽度不同
+      let tagValue = this.tagValue
+      if (tagValue && tagValue.length) {
+        let reLen = 0
+        for (let i = 0; i < tagValue.length; i++) {
+          if (tagValue.charCodeAt(i) >= 0 && tagValue.charCodeAt(i) <= 128) {
+            reLen += 0.5
+          } else {
+            reLen += 1
+          }
+        }
+        return reLen + 1
+      } else {
+        return 1
+      }
+    },
+    inputListeners () {
+      let vm = this
+      return Object.assign({}, this.$listeners, {
+        input (e) {
+          vm.$emit('input', e.target.value)
+        },
+        blur () {
+          if (vm.value === '') vm.emitDeleteTag()
+        }
+      })
+    }
+  },
+  methods: {
+    emitDeleteTag () {
+      this.$emit('deleteTag')
+    }
+  }
+}
+</script>
+
+<style scoped>
+.tag-input-wrapper {
+  display: inline-block;
+  margin: 5px;
+}
+.tag-input {
+  border: none;
+  background: #addaf9;
+  color: #222;
+  outline: none;
+  border-radius: 3px;
+  padding: 3px 5px;
+  min-width: 1.5em;
+  font-size: 13px;
+  font-family: Consolas, monospace;
+}
+.tag-input-icon {
+  color: #2790da;
+  margin-left: 5px;
+}
+.tag-input-icon:hover{
+  cursor: pointer;
+}
+</style>
